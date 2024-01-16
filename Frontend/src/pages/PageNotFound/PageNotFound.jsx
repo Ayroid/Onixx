d41aa76e4752c119a0096af5d0eb2ce5f129c25c
Ayroid <ayushsk0000@gmail.com>
2024-01-17 02:22:54 +0530

    404 Page designed

--- Frontend/src/pages/PageNotFound/PageNotFound.jsx
+++ Frontend/src/pages/PageNotFound/PageNotFound.jsx
@@ -1,5 +1,26 @@
+import styles from "./PageNotFound.module.css";
+import { useNavigate } from "react-router-dom";
+
+// CSS STYLES
+const { mainDiv, heading, subHeading, button } = styles;
+
 const PageNotFound = () => {
-  return <div>404</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={mainDiv}>
+      <h1 className={heading}>404</h1>
+      <h2 className={subHeading}>Page Not Found!</h2>
+      <button
+        className={button}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Back to Home
+      </button>
+    </div>
+  );
 };
 
 export default PageNotFound;
