130b7888b3e7a9d77bd3f5f822647783d30fa4fa
Ayroid <ayushsk0000@gmail.com>
2024-01-17 04:35:30 +0530

    useFetch hook created

---
+++ Frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = ({ url }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetch = () => {
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+        },
+      })
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetch();
+  });
+
+  return { data, loading, error, refetch: fetch };
+};
+
+export default useFetch;
